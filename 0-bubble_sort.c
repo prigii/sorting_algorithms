1057086c15f84e5eecf0517dcfe2e2b1eb156518
Peter Rigii <theonlyrigii@gmail.com>
2023-07-25 12:34:22 +0300

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,51 +1,29 @@
-#include <stdio.h>
+#include "sort.h"
 
-void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+/**
+  * bubble_sort - A function for bubble sorting.
+  * @array: The array to sort.
+  * @size: THe length of the aaray.
+  * Return: Nothing.
+  */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i = 0, j = 0;
+	int temp = 0;
+
+	if (array == NULL || size == 0)
+		return;
+	for (; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+		}
+	}
 }
-
-void bubble_sort(int *array, size_t size) {
-    int swapped;
-    for (size_t i = 0; i < size - 1; i++) {
-        swapped = 0;
-        for (size_t j = 0; j < size - i - 1; j++) {
-            if (array[j] > array[j + 1]) {
-                swap(&array[j], &array[j + 1]);
-                swapped = 1;
-            }
-        }
-        if (!swapped) // If no two elements were swapped in the inner loop, the array is already sorted.
-            break;
-
-        // Print the array after each pass
-        printf("After pass %zu: ", i + 1);
-        for (size_t k = 0; k < size; k++) {
-            printf("%d ", array[k]);
-        }
-        printf("\n");
-    }
-}
-
-int main() {
-    int array[] = {64, 34, 25, 12, 22, 11, 90};
-    size_t size = sizeof(array) / sizeof(array[0]);
-
-    printf("Original array: ");
-    for (size_t i = 0; i < size; i++) {
-        printf("%d ", array[i]);
-    }
-    printf("\n");
-
-    bubble_sort(array, size);
-
-    printf("Sorted array: ");
-    for (size_t i = 0; i < size; i++) {
-        printf("%d ", array[i]);
-    }
-    printf("\n");
-
-    return 0;
-}
-
